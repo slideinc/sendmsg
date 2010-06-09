51d0280e396f46f42bf9413649600deecb1c44f7
Travis J Parker <travis.parker@gmail.com>
2010-06-09 16:06:17 -0700

    get the packaging stuff started

---
+++ pavement.py
@@ -0,0 +1,60 @@
+import errno
+import os
+from setuptools import Extension
+
+from paver.easy import *
+from paver.path import path
+from paver.setuputils import setup
+
+
+setup(
+    name="sendmsg",
+    description="send file descriptors over AF_UNIX sockets via sendmsg(2)",
+    version="1.0",
+    ext_modules=[Extension(
+        'sendmsg',
+        ['sendmsg.c'],
+        include_dirs=('.',),
+        extra_compile_args=['-Wall'])],
+    classifiers = [
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        #"License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Operating System :: Unix",
+        "Programming Language :: C",
+        "Topic :: System :: Networking",
+    ]
+)
+
+MANIFEST = (
+    "setup.py",
+    "paver-minilib.zip",
+    "sendmsg.c",
+)
+
+@task
+def manifest():
+    path('MANIFEST.in').write_lines('include %s' % x for x in MANIFEST)
+
+@task
+@needs('generate_setup', 'minilib', 'manifest', 'setuptools.command.sdist')
+def sdist():
+    pass
+
+@task
+def clean():
+    for p in map(path, ('sendmsg.egg-info', 'dist', 'build', 'MANIFEST.in')):
+        if p.exists():
+            if p.isdir():
+                p.rmtree()
+            else:
+                p.remove()
+    for p in path(__file__).abspath().parent.walkfiles():
+        if p.endswith(".pyc") or p.endswith(".pyo"):
+            try:
+                p.remove()
+            except OSError, exc:
+                if exc.args[0] == errno.EACCES:
+                    continue
+                raise
